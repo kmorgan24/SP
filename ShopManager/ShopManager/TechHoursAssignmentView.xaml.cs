53f16d610e05012295aeaa0032d2ac66d3a96dae
Unknown <kyronnr@gmail.com>
2018-03-09 10:00:59 -0800

    UI Setup for Assigning Hours

---
+++ ShopManager/ShopManager/TechHoursAssignmentView.xaml.cs
@@ -0,0 +1,38 @@
+﻿using ShopManagerClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ShopManager
+{
+    /// <summary>
+    /// Interaction logic for TechHoursAssignmentView.xaml
+    /// </summary>
+    public partial class TechHoursAssignmentView : UserControl
+    {
+        public TechHoursAssignmentView()
+        {
+            InitializeComponent();
+        }
+        public TechHoursAssignmentView(long jobHours, long maxHours, long orderID, long techID, List<WorkOrder> currentAssignments)
+        {
+            InitializeComponent();
+        }
+
+        private void SelectBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
